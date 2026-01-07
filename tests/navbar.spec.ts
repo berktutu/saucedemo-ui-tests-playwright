41dddd0ddd23d78f1cc3c75ecaa6320d7c4970f7
berktutu <brktutu@gmail.com>
2026-01-07 20:58:14 +0300

    Automated the last P1 navbar test case

--- tests/navbar.spec.ts
+++ tests/navbar.spec.ts
@@ -2,13 +2,19 @@ import { test, expect } from "@playwright/test";
 import { Navbar } from "../page-objects/navbar";
 import { links } from "../test-data/testData";
 import { loginAsValidUser } from "../helpers/login-helper";
+import { ProductsPage } from "../page-objects/productsPage";
+import { ProductDetailPage } from "../page-objects/productDetail";
 
 let navbar: Navbar;
+let productsPage: ProductsPage;
+let productDetailPage: ProductDetailPage;
 
 test.beforeEach(async ({ page }) => {
   await page.goto(links.mainLink);
   await loginAsValidUser(page);
   navbar = new Navbar(page);
+  productsPage = new ProductsPage(page);
+  productDetailPage = new ProductDetailPage(page);
 });
 
 test.describe("Navbar Tests", () => {
@@ -36,5 +42,20 @@ test.describe("Navbar Tests", () => {
     await navbar.openNavMenu();
 
     await expect(navbar.logoutOption).toBeVisible();
+  });
+
+  test("Navigate to Products page using the navbar", async ({ page }) => {
+    await productsPage.getProductTitle().click();
+
+    await navbar.openNavMenu();
+
+    await expect(navbar.navbarMenuSlider).toHaveAttribute(
+      "aria-hidden",
+      "false"
+    );
+
+    await navbar.allItemsOption.click();
+
+    await expect(page).toHaveURL(links.productsPageLink);
   });
 });
